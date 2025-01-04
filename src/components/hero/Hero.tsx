d2b4ba926581bd8589b191eef854a8f77b78f4e0
A <iworkfortech@gmail.com>
2025-01-04 16:02:41 +0530

    Create basic structure of Hero section.

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,17 +1,21 @@
 export const Hero = () => {
   return (
-    <section className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-black to-yellow-200 py-16 text-center'>
+    <section className='flex min-h-screen flex-col items-center justify-center space-y-32 bg-gradient-to-b from-slate-900 via-slate-800 to-yellow-100 py-16 text-center'>
       {/* Heading */}
-      <h1 className='text-4xl font-bold text-white sm:text-5xl'>
-        Let’s <span className='text-yellow-500'>Connect</span>
-      </h1>
+      <div className='space-y-24'>
+        <h1 className='text-8xl font-bold text-white'>
+          Let’s <span className='text-yellow-500'>Connect</span>
+        </h1>
 
-      {/* Subheading */}
-      <p className='mt-4 max-w-xl text-gray-300'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy
-        eirmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed diam nonumy eirmod tempor.
-      </p>
+        {/* Subheading */}
+        <p className='mt-4 max-w-7xl text-xl text-gray-300'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
+          nonumy eirmod tempor Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit, sed diam nonumy eirmod tempor.
+        </p>
+      </div>
+
+      <div>circle round</div>
     </section>
   );
 };
