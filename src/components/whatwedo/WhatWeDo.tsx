d1b0ed94060270159920a7041d5c5465850d3b0c
A <iworkfortech@gmail.com>
2025-01-08 02:18:50 +0530

    Fix styles in hero section.

--- src/components/whatwedo/WhatWeDo.tsx
+++ src/components/whatwedo/WhatWeDo.tsx
@@ -31,30 +31,32 @@ export const WhatWeDo = () => {
   ];
 
   return (
-    <section className='flex flex-col items-center justify-center space-y-24 rounded-[80px] bg-white px-12 py-24'>
-      {/* Heading */}
-      <div className='text-center'>
-        <SectionHeader
-          title={'What we do?'}
-          subtitle={
-            'Lorem ipsum dolor sit amet, consectetur sadipscing elitr, sed diam\n' +
-            '          nonumy eirmod tempor Lorem ipsum dolor sit amet.'
-          }
-          className={'gap-4'}
-        />
-      </div>
-
-      {/* Cards */}
-      <div className='grid scale-110 grid-cols-1 gap-20 sm:grid-cols-2 lg:gap-x-24'>
-        {cards.map((card, index) => (
-          <Card
-            key={index}
-            icon={card.icon}
-            title={card.title}
-            description={card.description}
+    <div className='relative grid min-h-[70vh] place-items-center'>
+      <section className='absolute -top-16 flex w-full flex-col items-center justify-center space-y-24 rounded-[80px] bg-white px-12 py-24'>
+        {/* Heading */}
+        <div className='text-center'>
+          <SectionHeader
+            title={'What we do?'}
+            subtitle={
+              'Lorem ipsum dolor sit amet, consectetur sadipscing elitr, sed diam\n' +
+              '          nonumy eirmod tempor Lorem ipsum dolor sit amet.'
+            }
+            className={'gap-4'}
           />
-        ))}
-      </div>
-    </section>
+        </div>
+
+        {/* Cards */}
+        <div className='grid scale-110 grid-cols-1 gap-20 sm:grid-cols-2 lg:gap-x-24'>
+          {cards.map((card, index) => (
+            <Card
+              key={index}
+              icon={card.icon}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
