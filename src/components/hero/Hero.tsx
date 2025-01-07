6567717954a2710b517435f5427ace8a27e01649
A <iworkfortech@gmail.com>
2025-01-08 02:14:18 +0530

    Fix styles.

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,7 +3,7 @@ import { Button } from '../button/Button.tsx';
 
 export const Hero = () => {
   return (
-    <section className='flex min-h-screen flex-col items-center justify-center gap-52 space-y-32 bg-gradient-to-b from-slate-900 via-slate-800 to-yellow-100 py-16 text-center'>
+    <section className='flex min-h-screen flex-col items-center justify-center gap-52 space-y-32 bg-gradient-to-b from-slate-900 via-slate-800 to-yellow-100 py-16 pt-32 text-center'>
       {/* Heading */}
       <div className='space-y-24'>
         <h1 className='text-8xl font-bold text-white'>
@@ -11,7 +11,7 @@ export const Hero = () => {
         </h1>
 
         {/* Subheading */}
-        <p className='mt-4 max-w-7xl text-xl text-gray-300'>
+        <p className='mt-4 max-w-5xl text-xl text-gray-300'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
           nonumy eirmod tempor Lorem ipsum dolor sit amet, consectetur
           adipiscing elit, sed diam nonumy eirmod tempor.
