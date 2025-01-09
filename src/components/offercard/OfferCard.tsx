a1264ad709995d9d06355e9c65ebdecd4b4b7319
A <iworkfortech@gmail.com>
2025-01-09 23:31:38 +0530

    Change style for offer card.

--- src/components/offercard/OfferCard.tsx
+++ src/components/offercard/OfferCard.tsx
@@ -12,12 +12,12 @@ export const OfferCard = ({
   gradientClass,
 }: OfferCardProps) => {
   return (
-    <div className='flex h-[450px] flex-col overflow-hidden rounded-[60px] bg-white shadow-md'>
+    <div className='flex h-[500px] flex-col overflow-hidden rounded-[60px] bg-white shadow-md'>
       {/* Gradient Top Section */}
       <div className={`relative h-[60%] ${gradientClass}`}>
         {/* Circular Link Icon */}
         <div className='absolute right-6 top-6 flex flex-col items-center'>
-          <div className='flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-lg'>
+          <div className='flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-lg transition-colors hover:bg-slate-800 hover:text-white'>
             <Link />
           </div>
           <p className='text-white'>Connect</p>
@@ -25,9 +25,9 @@ export const OfferCard = ({
       </div>
 
       {/* Content Section */}
-      <div className='grid h-[40%] place-items-center px-12'>
+      <div className='grid h-[40%] scale-110 place-items-center px-12'>
         <div className='text-left'>
-          <h3 className='text-lg font-semibold text-gray-800'>{title}</h3>
+          <p className='text-lg font-semibold text-gray-800'>{title}</p>
           <p className='text-sm text-gray-600'>{description}</p>
         </div>
       </div>
--- src/components/whatweoffer/WhatWeOffer.tsx
+++ src/components/whatweoffer/WhatWeOffer.tsx
@@ -26,7 +26,7 @@ export const WhatWeOffer = () => {
   ];
 
   return (
-    <section className='z-20 space-y-32 bg-slate-100 py-16 pt-48'>
+    <section className='z-20 space-y-20 bg-slate-100 py-16 pt-48'>
       {/* Heading */}
       <div className='mb-12 text-center'>
         <SectionHeader
