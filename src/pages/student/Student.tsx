b5eb6120fd23eddb58608e1bbe20d8c61da919df
A <iworkfortech@gmail.com>
2025-01-12 03:41:29 +0530

    Add other components to the page.

--- src/pages/student/Student.tsx
+++ src/pages/student/Student.tsx
@@ -1,11 +1,23 @@
 import { QuestionsWeGet } from '../../components/questions-we-get/QuestionsWeGet.tsx';
 import { StudentHero } from '../../components/student-hero/StudentHero.tsx';
+import { WhatWeDo } from '../../components/whatwedo/WhatWeDo.tsx';
+import { WhyChooseUs } from '../../components/whychooseus/WhyChooseUs.tsx';
+import { StudentMentorConnect } from '../../components/studentmentorconnect/StudentMentorConnect.tsx';
+import { OurEvents } from '../../components/our-events/OurEvents.tsx';
+import { Testimonials } from '../../components/testimonials/Testimonials.tsx';
+import { FaqSection } from '../../components/faqsection/FaqSection.tsx';
 
 export const Student = () => {
   return (
     <main className={''}>
       <StudentHero />
+      <WhatWeDo />
+      <WhyChooseUs />
+      <StudentMentorConnect />
+      <OurEvents />
       <QuestionsWeGet />
+      <Testimonials />
+      <FaqSection />
     </main>
   );
 };
