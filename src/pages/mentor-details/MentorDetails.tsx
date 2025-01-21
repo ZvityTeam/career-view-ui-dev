fadeab4a1d56ee2d8c7a75757c0a22d672ef8881
A <iworkfortech@gmail.com>
2025-01-22 01:29:00 +0530

    Fix styling according to UX.

--- src/pages/mentor-details/MentorDetails.tsx
+++ src/pages/mentor-details/MentorDetails.tsx
@@ -1,12 +1,20 @@
 import { useParams } from 'react-router-dom';
 import { mentorData } from '../../components/mentors-list';
 import { MentorProfileProps } from '../../components/mentor-profile-card';
+import { useNavbarContext } from '../../context/navbar-context/NavbarContext.tsx';
+import { useEffect } from 'react';
+import { Section } from '../../components/container/Section.tsx';
 
 export const MentorDetails = () => {
   const { id } = useParams<string>();
+  const { setNavbarTheme } = useNavbarContext();
+  useEffect(() => {
+    setNavbarTheme(true);
+  }, [setNavbarTheme]);
+
   const mentor = mentorData[id as unknown as number];
   return (
-    <main>
+    <main className={'mt-32'}>
       <Header {...mentor} />
     </main>
   );
@@ -16,17 +24,22 @@ type HeaderProps = Pick<MentorProfileProps, 'name' | 'profileImage' | 'bio'>;
 
 const Header = ({ name, profileImage, bio }: HeaderProps) => {
   return (
-    <section className={'relative'}>
-      <div
-        className={'absolute left-1/2 top-1/2 translate-x-1/2 translate-y-1/2'}
-      >
+    <Section className={'flex min-h-72 w-screen flex-col gap-20'}>
+      <div className='relative w-full'>
+        <img
+          src={'https://placehold.co/600x50'}
+          className='w-full object-contain'
+        />
         <img
           src={profileImage}
+          className='absolute -bottom-1/2 left-1/2 -translate-x-1/2 rounded-full border-2 border-white'
           alt={name}
         />
       </div>
-      <div>{bio}</div>
-      <div></div>
-    </section>
+      <div className={'flex flex-col items-center gap-2'}>
+        <p className={'text-4xl font-bold'}>{name}</p>
+        <p className={'text-2xl font-[100] italic'}>{bio}</p>
+      </div>
+    </Section>
   );
 };
