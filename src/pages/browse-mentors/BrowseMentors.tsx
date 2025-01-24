2f1d3ac0e5e51572f8700235d5fba1262a74d38c
A <iworkfortech@gmail.com>
2025-01-25 03:49:15 +0530

    Added search bar.

--- src/pages/browse-mentors/BrowseMentors.tsx
+++ src/pages/browse-mentors/BrowseMentors.tsx
@@ -2,11 +2,13 @@ import { MentorsList } from '../../components/mentors-list';
 import { Testimonials } from '../../components/testimonials/Testimonials.tsx';
 import { FaqSection } from '../../components/faqsection/FaqSection.tsx';
 import { ContactForm } from '../../components/contact-form';
+import MentorSearch from '../../components/mentor-search';
 
 export const BrowseMentors = () => {
   return (
     <main>
       <div className={'min-h-screen bg-slate-950'} />
+      <MentorSearch />
       <MentorsList />
       <Testimonials />
       <ContactForm />
