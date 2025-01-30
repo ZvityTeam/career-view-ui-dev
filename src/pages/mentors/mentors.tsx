da531cd51f472dd0b49622587c4677f85be1064a
A <iworkfortech@gmail.com>
2025-01-31 03:04:15 +0530

    Added logo section.

--- src/pages/mentors/mentors.tsx
+++ src/pages/mentors/mentors.tsx
@@ -4,12 +4,14 @@ import { Testimonials } from '../../components/testimonials/Testimonials.tsx';
 import { FaqSection } from '../../components/faqsection/FaqSection.tsx';
 import { MentorHero } from '../../components/mentor-hero.tsx';
 import { VerticalTimelineComponent } from '../../components/vertical-timeline.tsx';
+import LogoScroll from '../../components/logo-scroll.tsx';
 
 export const Mentors = () => {
   return (
     <main>
       <MentorHero />
       <WhatToExpect />
+      <LogoScroll />
       <VerticalTimelineComponent />
       <SchoolConnectCTA />
       <Testimonials />
