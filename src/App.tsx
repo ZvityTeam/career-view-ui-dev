9ad469a65cabe1e4842eb78f92c388deba69b7b4
A <iworkfortech@gmail.com>
2025-01-31 02:21:56 +0530

    Added global setting to scroll to top when page loads.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Navbar } from './components/navbar/Navbar.tsx';
 import { Footer } from './components/footer/Footer.tsx';
 import { Providers } from './context/Providers.tsx';
 import { SavedMentors } from './components/saved-mentor';
+import { ScrollToTop } from './components/ui/scroll-to-top.tsx';
 
 function App() {
   return (
     <>
       <Providers>
+        <ScrollToTop />
         <Navbar />
         <AppRouter />
         <Footer />
