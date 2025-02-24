c63c468102a90717f4d7649385910bacd7f456a7
A <iworkfortech@gmail.com>
2025-02-25 02:07:58 +0530

    Update Footer component to enhance background visuals and add decorative elements for improved aesthetics

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,6 +3,9 @@ import { OutlinedInputWithButton } from '../input-box/InputBox';
 import { Send } from 'lucide-react';
 import { FOOTER_NAV_ITEMS } from '../navbar/NavbarConfig.ts';
 import { FooterLink } from './footerlink/FooterLink.tsx';
+import bg1 from '../svgs/bg-1.svg';
+import bg2 from '../svgs/bg-2.svg';
+import star from '../svgs/footer-star.svg';
 
 interface FooterProps {
   /** Optional: You can pass in a background image or just rely on a CSS background */
@@ -11,11 +14,21 @@ interface FooterProps {
 
 export const Footer: React.FC<FooterProps> = () => {
   return (
-    <footer className='w-full bg-[linear-gradient(165deg,_#000000,_#272727,_#6E6E6E)] text-white'>
+    <footer className='relative w-full overflow-hidden bg-[linear-gradient(165deg,_#000000,_#272727,_#6E6E6E)] text-white'>
+      <img
+        src={bg1}
+        alt={''}
+        className={'pointer-events-none absolute -left-[10%] bottom-0 z-10'}
+      />
+      <img
+        src={bg2}
+        alt={''}
+        className={'pointer-events-none absolute right-0 top-0 z-10'}
+      />
       <div className='flex min-h-screen justify-between px-44'>
         <div className='flex max-w-2xl flex-col gap-7 p-24'>
           {/* Multiline heading */}
-          <h4 className='text-5xl font-bold leading-tight'>
+          <h4 className='z-20 text-5xl font-bold leading-tight'>
             <span className='relative'>
               <span className='absolute -left-[70%] italic'>Let’s</span>Connect
             </span>
@@ -25,7 +38,7 @@ export const Footer: React.FC<FooterProps> = () => {
           </h4>
 
           {/* Subtext */}
-          <p className='text-lg text-gray-200'>
+          <p className='z-20 text-lg text-gray-200'>
             Join our weekly newsletter for e-books, live sessions and event
             updates
           </p>
@@ -39,7 +52,7 @@ export const Footer: React.FC<FooterProps> = () => {
           />
 
           {/* Policy Links */}
-          <div className='mt-12 flex gap-16 text-2xl underline'>
+          <div className='z-20 mt-12 flex gap-16 text-2xl underline'>
             <FooterLink
               to='/privacy-policy'
               label='Privacy Policy'
@@ -52,7 +65,7 @@ export const Footer: React.FC<FooterProps> = () => {
         </div>
 
         {/* Navigation Links */}
-        <div className='flex flex-col gap-8 p-24 text-right text-3xl'>
+        <div className='z-20 flex flex-col gap-8 p-24 text-right text-3xl'>
           {FOOTER_NAV_ITEMS.map((item, index) => (
             <FooterLink
               key={index}
@@ -63,8 +76,15 @@ export const Footer: React.FC<FooterProps> = () => {
         </div>
       </div>
 
-      <div className='grid place-items-center'>
-        <h4 className='text-[200px]'>CareerView</h4>
+      <div className='z-20 grid place-items-center'>
+        <h4 className='relative z-20 text-[200px]'>
+          <img
+            src={star}
+            alt={''}
+            className={'pointer-events-none absolute -top-1/3 left-0 z-10'}
+          />
+          CareerView
+        </h4>
       </div>
     </footer>
   );
