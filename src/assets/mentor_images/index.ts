57e7804eb92e9beee148bd260b522359076fcc38
A <iworkfortech@gmail.com>
2025-03-01 04:02:09 +0530

    Add image exports for multiple user profiles

---
+++ src/assets/mentor_images/index.ts
@@ -0,0 +1,89 @@
+export { default as alex_mirco } from './alex_mirco.png';
+export { default as david_lu } from './david_lu.png';
+export { default as holly_thong } from './holly_thong.png';
+export { default as joshua_bell } from './joshua_bell.png';
+export { default as nicolas_floan } from './nicolas_floan.png';
+export { default as steve_rivas } from './steve_rivas.png';
+
+export { default as ali_nasiriy } from './ali_nasiriy.png';
+export { default as dien_nguyen } from './dien_nguyen.png';
+export { default as izzy_rudy } from './izzy_rudy.png';
+export { default as joyti_mabruk } from './joyti_mabruk.png';
+export { default as paris_ward } from './paris_ward.png';
+export { default as tammy_lee } from './tammy_lee.png';
+
+export { default as alice_huynh_tran } from './alice_huynh_tran.png';
+export { default as edward_sawle } from './edward_sawle.png';
+export { default as jack_haladane } from './jack_haladane.png';
+export { default as kristina_constantinou } from './kristina_constantinou.png';
+export { default as peter_wu } from './peter_wu.png';
+export { default as thang_le } from './thang_le.png';
+
+export { default as andrea_mcclure } from './andrea_mcclure.png';
+export { default as elishia_dunne } from './elishia_dunne.png';
+export { default as jack_hallam } from './jack_hallam.png';
+export { default as lara_urosevic } from './lara_urosevic.png';
+export { default as pranab_pai } from './pranab_pai.png';
+export { default as thomas_clifton } from './thomas_clifton.png';
+
+export { default as andrew_korol } from './andrew_korol.png';
+export { default as ella_burnage } from './ella_burnage.png';
+export { default as jack_seprarovic } from './jack_seprarovic.png';
+export { default as lauren_dornan } from './lauren_dornan.png';
+export { default as raman_kahlon } from './raman_kahlon.png';
+export { default as timothy_ly } from './timothy_ly.png';
+
+export { default as anesu_dumba } from './anesu_dumba.png';
+export { default as elliot_charleston } from './elliot_charleston.png';
+export { default as jacky_lam } from './jacky_lam.png';
+export { default as libby_perica } from './libby_perica.png';
+export { default as rebekah_jones } from './rebekah_jones.png';
+export { default as tom_payton } from './tom_payton.png';
+
+export { default as ben_manifold } from './ben_manifold.png';
+export { default as emma_wilkins_brittain } from './emma_wilkins_brittain.png';
+export { default as james_fiori } from './james_fiori.png';
+export { default as logan_dongray } from './logan_dongray.png';
+export { default as roxanne_magniet } from './roxanne_magniet.png';
+export { default as usaid_rana } from './usaid_rana.png';
+
+export { default as brian_liu } from './brian_liu.png';
+export { default as evan_o_keefe } from './evan_o_keefe.png';
+export { default as jarrod_morton } from './jarrod_morton.png';
+export { default as manu_nair } from './manu_nair.png';
+export { default as sam_fatovich } from './sam_fatovich.png';
+export { default as vanessa_liew } from './vanessa_liew.png';
+
+export { default as bronwen_prinsloo } from './bronwen_prinsloo.png';
+export { default as gabrielle_percic } from './gabrielle_percic.png';
+export { default as jason_wong } from './jason_wong.png';
+export { default as megan_aspinall } from './megan_aspinall.png';
+export { default as samuel_skuthorp } from './samuel_skuthorp.png';
+export { default as vatsla_trivedi } from './vatsla_trivedi.png';
+
+export { default as callum_smith } from './callum_smith.png';
+export { default as georgia_parentich } from './georgia_parentich.png';
+export { default as jessica_chia } from './jessica_chia.png';
+export { default as merryl_sequeira } from './merryl_sequeira.png';
+export { default as serena_rodrigues } from './serena_rodrigues.png';
+export { default as zach_werner } from './zach_werner.png';
+
+export { default as calum_macdonald } from './calum_macdonald.png';
+export { default as grace_lee } from './grace_lee.png';
+export { default as jessie_somerville } from './jessie_somerville.png';
+export { default as mishna_nagda } from './mishna_nagda.png';
+export { default as shaun_chambers } from './shaun_chambers.png';
+export { default as zubair_shariff } from './zubair_shariff.png';
+
+export { default as christian_portelli } from './christian_portelli.png';
+export { default as hannah_ngo } from './hannah_ngo.png';
+export { default as jordan_randazzo } from './jordan_randazzo.png';
+export { default as mitch_terry } from './mitch_terry.png';
+export { default as simone_hammond } from './simone_hammond.png';
+
+export { default as claudio_pizzirani } from './claudio_pizzirani.png';
+export { default as hannah_nichol } from './hannah_nichol.png';
+export { default as jordy_skye } from './jordy_skye.png';
+export { default as nagham_sakhr } from './nagham_sakhr.png';
+export { default as darren_sweeney } from './darren_sweeney.png';
+export { default as helene_lesaffre } from './helene_lesaffre.png';
