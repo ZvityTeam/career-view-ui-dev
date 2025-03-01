869f1005d7ecb66307457f33fc24d171923a60eb
A <iworkfortech@gmail.com>
2025-03-02 02:58:41 +0530

    Refactor mentor data handling by moving mentor initialization to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ import { Footer } from './components/footer/Footer.tsx';
 import { Providers } from './context/Providers.tsx';
 import { SavedMentors } from './components/saved-mentor';
 import { ScrollToTop } from './components/ui/scroll-to-top.tsx';
+import { useMentorStore } from './store/useMentorStore.ts';
+import { useEffect } from 'react';
+import { data as mentorsData } from './content/mentors.ts';
 
 function App() {
+  const setMentors = useMentorStore((state) => state.setMentors);
+
+  useEffect(() => {
+    setMentors(mentorsData);
+  }, [setMentors]);
   return (
     <>
       <Providers>
--- src/pages/browse-mentors/BrowseMentors.tsx
+++ src/pages/browse-mentors/BrowseMentors.tsx
@@ -3,18 +3,9 @@ import { Testimonials } from '../../components/testimonials/Testimonials.tsx';
 import { FaqSection } from '../../components/faqsection/FaqSection.tsx';
 import MentorSearch from '../../components/mentor-search';
 import { BrowseMentorsHero } from '../../components/browse-mentors-hero.tsx';
-import { useMentorStore } from '../../store/useMentorStore.ts';
-import { useEffect } from 'react';
-import { data as mentorsData } from '../../content/mentors';
 import { AnimatedPageWrapper } from '../../components/PageWrapper.tsx';
 
 export const BrowseMentors = () => {
-  const setMentors = useMentorStore((state) => state.setMentors);
-
-  useEffect(() => {
-    setMentors(mentorsData);
-  }, [setMentors]);
-
   return (
     <AnimatedPageWrapper>
       <main>
