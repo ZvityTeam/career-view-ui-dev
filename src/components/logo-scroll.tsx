f6921d5f57e9c55b514a65b49553dc3c16504f25
Sanjeev Chitlapalli <team.zvity@gmail.com>
2025-05-26 20:34:07 +0530

    revert max width styles for logos

--- src/components/logo-scroll.tsx
+++ src/components/logo-scroll.tsx
@@ -59,7 +59,7 @@ const LogoItem = ({ src }: { src: string }) => {
     <img
       src={src}
       alt='logo'
-      className='mx-4 h-auto sm:mx-6 sm:max-w-sm lg:mx-10 lg:max-w-xl'
+      className='mx-4 h-auto max-w-xs sm:mx-6 sm:max-w-md lg:mx-10 lg:max-w-xl'
     />
   );
 };
