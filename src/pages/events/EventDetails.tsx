66e70d6b70b6dcb4ea439d6ca5d845ffa663fb75
Sanjeev Chitlapalli <ui.builder22@gmail.com>
2025-09-08 21:27:38 +0530

    feat(EventDetails): add school logo display above event time
    Display the school logo above the event time section to improve visual identification of the event's associated institution

--- src/pages/events/EventDetails.tsx
+++ src/pages/events/EventDetails.tsx
@@ -61,6 +61,11 @@ export const EventDetails = () => {
             <div className='mb-6 flex flex-col gap-4 md:flex-row md:items-center md:justify-between'>
               <div>
                 <p className='text-lg text-gray-700'>
+                  <img
+                    src={event.schoolLogo}
+                    alt={event.schoolName}
+                    className='h-12 mt-1 mb-5'
+                  />
                   <span className='font-semibold'>Date & Time:</span>{' '}
                   {eventTime}
                 </p>
